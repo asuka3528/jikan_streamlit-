801558d0e733bb15b71f6f0a2882610b83b56f58
asuka3528 <kn7ssaerkv284inpbe18s@gmail.com>
2023-08-09 22:18:21 +0900

    Added pulp to requirements and updated jikan.py

--- jikan.py
+++ jikan.py
@@ -22,7 +22,8 @@ Classroom_mobility = ["芸術","体育","情報","家庭科"] #移動教室授�
 six_period = ["総合探究","自主自学"] #6限のみの授業
 subject_dict = {s:n for s,n in zip(subject_list,[4,5,5,4,4,2,2,2,1,3])} #必要授業数
 
-
+# Excelを読み込む
+df = pd.read_excel("C:\Users\fssga\OneDrive\デスクトップ\時間割.xlsx")
 lesson_df = pd.read_csv("https://docs.google.com/spreadsheets/d/1nz31-E6E92Xzmw7JpUP6YQdc9UnYQcdb6OwWXQoDg7s/export?format=csv")
 
 
